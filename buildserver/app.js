f43883fa8b81fa82b47d969ed7f5fb0d5a439f6d
Ian Drosos <ian.drosos@gmail.com>
2016-09-24 11:48:38 -0400

    build scripts executed by express server

--- buildserver/app.js
+++ buildserver/app.js
@@ -1,18 +1,41 @@
 var express = require('express');
 var app = express();
+var exec = require('child_process').exec;
+var child;
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
 app.get('/dev', function (req, res) {
-        console.log('in dev get');
-        res.send('dev branch build path from post-commit');
+  console.log('in dev get');
+  child = exec("./scripts/build-dev", function (error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+      console.error('exec error: ' + error);
+      res.send('dev branch build ERROR (check logs)');
+    } else {
+      console.log('dev BUILD SUCCESSFUL (DO STUFF)');
+      res.send('dev branch build SUCCESS (do stuff)');
+    }
+  });
 });
 
 app.get('/release', function (req, res) {
-        console.log('in release get');
-        res.send('release branch build path from post-commit');
+  console.log('in release get');
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+      res.send('release branch build ERROR (check logs)');
+    } else {
+      console.log('release BUILD SUCCESSFUL (DO STUFF)');
+      res.send('release branch build SUCCESS (do stuff)');
+    }
+  });
+  res.send('release branch build path from post-commit');
 });
 
 app.listen(3000, function () {
