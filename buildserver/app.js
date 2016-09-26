79fe8580d199c53cacda0e7532f30c9f20aae814
build server <ian.drosos@gmail.com>
2016-09-26 20:07:19 +0000

    Creating logs for any kind of output.

--- buildserver/app.js
+++ buildserver/app.js
@@ -1,8 +1,11 @@
 var express = require('express');
-var app = express();
 var exec = require('child_process').exec;
-var child;
 var bodyParser = require('body-parser');
+var fs = require('fs');
+
+var app = express();
+var child;
+var serverLogFilePath = "logs/server.log";
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -10,77 +13,99 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+//processing for root request. put log list here
+app.get('/', function(req, res) {
+	fs.appendFileSync(serverLogFilePath, 'GET request for /. Will serve build history.\n');
+  res.send('DevOps Milestone 1: This is where you will get to see all the build logs.');
 });
 
-app.post('/postreceive', function (req, res) {
-  console.log('in postreceive post');
+//called by GitHub WebHook
+app.post('/postreceive', function(req, res) {
   var branch = req.body.ref;
-  console.log(branch);
+  var logFilePath = "logs/" + getCurrentTimeInISO() + ".log";
+
+  fs.writeFileSync(logFilePath, 'Build triggered from GitHub WebHook.\n');
+  fs.appendFileSync(logFilePath, 'Branch updated: ' + branch + "\n");
+  fs.appendFileSync(serverLogFilePath, 'POST request for /postreceive.\n');
+
   if (branch === "refs/heads/dev") {
-    child = exec("./scripts/build-dev", function (error, stdout, stderr) {
-      console.log('stdout: ' + stdout);
-      console.log('stderr: ' + stderr);
+    fs.appendFileSync(logFilePath, 'Will build dev branch.\n');
+    child = exec("./scripts/build-dev", function(error, stdout, stderr) {
+      fs.appendFileSync(logFilePath, 'Output in stdout: ' + stdout + "\n");
+      fs.appendFileSync(logFilePath, 'Output in stderr: ' + stderr + "\n");
       if (error !== null) {
-        console.log('exec error: ' + error);
-        console.error('exec error: ' + error);
-        res.send('dev branch build ERROR (check logs)');
+        fs.appendFileSync(logFilePath, 'exec error: ' + error + "\n");
+        res.send('dev branch build error (check logs).');
       } else {
-        console.log('dev BUILD SUCCESSFUL (DO STUFF)');
-        res.send('dev branch build SUCCESS (do stuff)');
+        fs.appendFileSync(logFilePath, 'dev branch build successful.\n');
+        res.send('dev branch build successful.');
       }
     });
   } else if (branch === "refs/heads/release") {
-    child = exec("./scripts/build-release", function (error, stdout, stderr) {
-      console.log('stdout: ' + stdout);
-      console.log('stderr: ' + stderr);
+    child = exec("./scripts/build-release", function(error, stdout, stderr) {
+      fs.appendFileSync(logFilePath, 'Output in stdout: ' + stdout + "\n");
+      fs.appendFileSync(logFilePath, 'Output in stderr: ' + stderr + "\n");
       if (error !== null) {
-        console.log('exec error: ' + error);
-        res.send('release branch build ERROR (check logs)');
+        fs.appendFileSync(logFilePath, 'exec error: ' + error + "\n");
+        res.send('release branch build error (check logs).');
       } else {
-        console.log('release BUILD SUCCESSFUL (DO STUFF)');
-        res.send('release branch build SUCCESS (do stuff)');
+        fs.appendFileSync(logFilePath, 'release branch build successful.\n');
+        res.send('release branch build successful.');
       }
     });
   } else {
-    console.log("NOT IN ACCEPTABLE BRANCH, NO BUILD");
-    res.send("NOT DEV OR RELEASE, NO BUILD");
+    fs.appendFileSync(logFilePath, "Not in acceptable branch, no build will occur.\n");
+    res.send("Not in dev or release branch, no build will occur.");
   }
 });
 
-app.get('/dev', function (req, res) {
-  console.log('in dev get');
-  child = exec("./scripts/build-dev", function (error, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
+//called by post-commit hook for dev branch
+app.get('/dev', function(req, res) {
+  var logFilePath = "logs/" + getCurrentTimeInISO() + ".log";
+
+  fs.writeFileSync(logFilePath, 'Build triggered from post-commit for dev.\n');
+  fs.appendFileSync(serverLogFilePath, 'GET request for /dev.\n');
+
+  child = exec("./scripts/build-dev", function(error, stdout, stderr) {
+    fs.appendFileSync(logFilePath, 'Output in stdout: ' + stdout + "\n");
+    fs.appendFileSync(logFilePath, 'Output in stderr: ' + stderr + "\n");
     if (error !== null) {
-      console.log('exec error: ' + error);
-      console.error('exec error: ' + error);
-      res.send('dev branch build ERROR (check logs)');
+      fs.appendFileSync(logFilePath, 'exec error: ' + error + "\n");
+      res.send('dev branch build error (check logs).');
     } else {
-      console.log('dev BUILD SUCCESSFUL (DO STUFF)');
-      res.send('dev branch build SUCCESS (do stuff)');
+      fs.appendFileSync(logFilePath, 'dev branch build successful.\n');
+      res.send('dev branch build successful.');
     }
   });
 });
 
-app.get('/release', function (req, res) {
-  console.log('in release get');
-  child = exec("./scripts/build-release", function (error, stdout, stderr) {
-    console.log('stdout: ' + stdout);
-    console.log('stderr: ' + stderr);
+//called by post-commit hook for release branch
+app.get('/release', function(req, res) {
+  var logFilePath = "logs/" + getCurrentTimeInISO() + ".log";
+
+  fs.writeFileSync(logFilePath, 'Build triggered from post-commit for release.\n');
+  fs.appendFileSync(serverLogFilePath, 'GET request for /release.\n');
+
+  child = exec("./scripts/build-release", function(error, stdout, stderr) {
+    fs.appendFileSync(logFilePath, 'Output in stdout: ' + stdout + "\n");
+    fs.appendFileSync(logFilePath, 'Output in stderr: ' + stderr + "\n");
     if (error !== null) {
-      console.log('exec error: ' + error);
-      res.send('release branch build ERROR (check logs)');
+      fs.appendFileSync(logFilePath, 'exec error: ' + error + "\n");
+      res.send('release branch build error (check logs).');
     } else {
-      console.log('release BUILD SUCCESSFUL (DO STUFF)');
-      res.send('release branch build SUCCESS (do stuff)');
+      fs.appendFileSync(logFilePath, 'release branch build successful.\n');
+      res.send('release branch build successful.');
     }
   });
   res.send('release branch build path from post-commit');
 });
 
-app.listen(3000, function () {
-  console.log('Buildserver listening on port 3000!');
+//server will listen on port 3000
+app.listen(3000, function() {
+  fs.appendFileSync(serverLogFilePath, 'Buildserver started at ' + getCurrentTimeInISO() + '. Listening on port 3000.\n');
 });
+
+//helper function to get current time in ISO format
+function getCurrentTimeInISO() {
+  return (new Date()).toISOString();
+}
