99d18f0bd5a27075b903a7e3e5a6072433c6755d
build server <ian.drosos@gmail.com>
2016-09-27 03:55:33 +0000

    Adding build history on root GET request.

--- buildserver/app.js
+++ buildserver/app.js
@@ -2,21 +2,46 @@ var express = require('express');
 var exec = require('child_process').exec;
 var bodyParser = require('body-parser');
 var fs = require('fs');
+var path = require('path');
 
 var app = express();
 var child;
 var serverLogFilePath = "logs/server.log";
 
+//view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'logs')));
 
 //processing for root request. put log list here
 app.get('/', function(req, res) {
 	fs.appendFileSync(serverLogFilePath, 'GET request for /. Will serve build history.\n');
-  res.send('DevOps Milestone 1: This is where you will get to see all the build logs.');
+  var files = fs.readdirSync("logs/");
+  var data = [];
+  for (var file of files) {
+    if (file == 'server.log')
+      continue;
+    var date = (new Date(file.split(".log")[0])).toString().split("GMT")[0];
+    var link = file;
+    var temp = fs.readFileSync("logs/" + file, "utf8");
+    var branch = temp.indexOf("dev branch") != -1 ? "dev" : "release";
+    var status = temp.indexOf("build successful") != -1 ? "successful" : "failure";
+    data.push({
+      date: date,
+      link: link,
+      branch: branch,
+      status: status
+    });
+  }
+  res.render('index', { 
+    title: 'Build Server',
+    data: data 
+  });
 });
 
 //called by GitHub WebHook
@@ -30,11 +55,12 @@ app.post('/postreceive', function(req, res) {
 
   if (branch === "refs/heads/dev") {
     fs.appendFileSync(logFilePath, 'Will build dev branch.\n');
-    child = exec("./scripts/build-dev", function(error, stdout, stderr) {
-      fs.appendFileSync(logFilePath, 'Output in stdout: ' + stdout + "\n");
-      fs.appendFileSync(logFilePath, 'Output in stderr: ' + stderr + "\n");
+    child = exec("./scripts/build_dev", function(error, stdout, stderr) {
+      fs.appendFileSync(logFilePath, '\nOutput in stdout:\n ' + stdout + "\n");
+      fs.appendFileSync(logFilePath, '\nOutput in stderr: \n' + stderr + "\n");
       if (error !== null) {
         fs.appendFileSync(logFilePath, 'exec error: ' + error + "\n");
+        fs.appendFileSync(logFilePath, 'dev branch build error.\n');
         res.send('dev branch build error (check logs).');
       } else {
         fs.appendFileSync(logFilePath, 'dev branch build successful.\n');
@@ -42,11 +68,12 @@ app.post('/postreceive', function(req, res) {
       }
     });
   } else if (branch === "refs/heads/release") {
-    child = exec("./scripts/build-release", function(error, stdout, stderr) {
+    child = exec("./scripts/build_release", function(error, stdout, stderr) {
       fs.appendFileSync(logFilePath, 'Output in stdout: ' + stdout + "\n");
       fs.appendFileSync(logFilePath, 'Output in stderr: ' + stderr + "\n");
       if (error !== null) {
         fs.appendFileSync(logFilePath, 'exec error: ' + error + "\n");
+        fs.appendFileSync(logFilePath, 'release branch build error.\n');
         res.send('release branch build error (check logs).');
       } else {
         fs.appendFileSync(logFilePath, 'release branch build successful.\n');
@@ -66,11 +93,12 @@ app.get('/dev', function(req, res) {
   fs.writeFileSync(logFilePath, 'Build triggered from post-commit for dev.\n');
   fs.appendFileSync(serverLogFilePath, 'GET request for /dev.\n');
 
-  child = exec("./scripts/build-dev", function(error, stdout, stderr) {
+  child = exec("./scripts/build_dev", function(error, stdout, stderr) {
     fs.appendFileSync(logFilePath, 'Output in stdout: ' + stdout + "\n");
     fs.appendFileSync(logFilePath, 'Output in stderr: ' + stderr + "\n");
     if (error !== null) {
       fs.appendFileSync(logFilePath, 'exec error: ' + error + "\n");
+      fs.appendFileSync(logFilePath, 'dev branch build error.\n');
       res.send('dev branch build error (check logs).');
     } else {
       fs.appendFileSync(logFilePath, 'dev branch build successful.\n');
@@ -86,11 +114,12 @@ app.get('/release', function(req, res) {
   fs.writeFileSync(logFilePath, 'Build triggered from post-commit for release.\n');
   fs.appendFileSync(serverLogFilePath, 'GET request for /release.\n');
 
-  child = exec("./scripts/build-release", function(error, stdout, stderr) {
+  child = exec("./scripts/build_release", function(error, stdout, stderr) {
     fs.appendFileSync(logFilePath, 'Output in stdout: ' + stdout + "\n");
     fs.appendFileSync(logFilePath, 'Output in stderr: ' + stderr + "\n");
     if (error !== null) {
       fs.appendFileSync(logFilePath, 'exec error: ' + error + "\n");
+      fs.appendFileSync(logFilePath, 'release branch build error.\n');
       res.send('release branch build error (check logs).');
     } else {
       fs.appendFileSync(logFilePath, 'release branch build successful.\n');
