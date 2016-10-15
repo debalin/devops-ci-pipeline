e6a026256f0b5ff24ecf32521d30cc2a9a5ea012
Ian Drosos <ian.drosos@gmail.com>
2016-10-14 20:36:25 -0400

    converting to react-markdown
    TODO logfile and custom test

---
+++ buildserver/scripts/run_tests.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# MAKE SURE TO CHMOD 0755 THESE SO SERVER CAN EXECUTE THEM
+# test script to run branch dev
+# ian drosos
+
+echo "Running test script.\n"
+npm --prefix /home/ubuntu/react-markdown test
