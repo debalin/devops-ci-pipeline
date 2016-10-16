fbc4da0e58a7de48f041a4bde73b85f20b0a263c
Pratik Mukherjee <pmukher@ncsu.edu>
2016-10-15 22:59:05 -0400

    Adding the static_code_analysis script

---
+++ buildserver/scripts/static_code_analysis.sh
@@ -0,0 +1,4 @@
+touch static_analysis_results.txt
+chmod 777 static_analysis_results.txt
+jshint react-markdown/src/react-markdown.js > static_analysis_results.txt
+#jsHint Hello.js > static_analysis_results.txt
