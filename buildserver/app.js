44debd37301c4196e69df0662080aba5b5305f0a
build server <ian.drosos@gmail.com>
2016-10-16 23:59:18 +0000

    Changing email structure + added coverage in UI.

--- buildserver/app.js
+++ buildserver/app.js
@@ -24,29 +24,36 @@ app.get('/', function(req, res) {
   fs.appendFileSync(serverLogFilePath, 'GET request for /. Will serve build history.\n');
   var dir = "logs/";
   var files = fs.readdirSync(dir);
+  //http://stackoverflow.com/questions/10559685/using-node-js-how-do-you-get-a-list-of-files-in-chronological-order
   files.sort(function(a, b) {
     return fs.statSync(dir + b).mtime.getTime() -
       fs.statSync(dir + a).mtime.getTime();
   });
   var data = [];
   for (var file of files) {
-    if (file == 'server.log' || file == 'tests.log' || !file.includes("build"))
+    if (file == 'server.log' || file == 'tests.log' || file == 'email.log' || !file.includes("build"))
       continue;
     var prefix = file.split("_build.log")[0];
     var date = (new Date(prefix)).toString().split("GMT")[0];
-    var buildlink = file;
-    var testlink = prefix + "_test.log";
-    var fuzzlink = prefix + "_fuzzingTest.log";
-    var staticlink = prefix + "_staticAnalysis.log";
-    var temp = fs.readFileSync("logs/" + buildlink, "utf8");
+    var buildLink = file;
+    var testLink = prefix + "_test.log";
+    var fuzzLink = prefix + "_fuzzingTest.log";
+    var staticLink = prefix + "_staticAnalysis.log";
+    var temp = fs.readFileSync("logs/" + buildLink, "utf8");
     var branch = temp.indexOf("dev branch") != -1 ? "dev" : "release";
     var buildstatus = temp.indexOf("build successful") != -1 ? "successful" : "failure";
-    var temp = fs.readFileSync("logs/" + testlink, "utf8");
+    var temp = fs.readFileSync("logs/" + testLink, "utf8");
     var teststatus = temp.indexOf("tests successful") != -1 ? "successful" : "failure";
-    var temp = fs.readFileSync("logs/" + fuzzlink, "utf8");
+    var temp = fs.readFileSync("logs/" + fuzzLink, "utf8");
     var fuzzstatus = temp.indexOf("fuzzing tests successful") != -1 ? "successful" : "failure";
-    var temp = fs.readFileSync("logs/" + staticlink, "utf8");
+    var temp = fs.readFileSync("logs/" + staticLink, "utf8");
     var staticstatus = temp.indexOf("static analysis successful") != -1 ? "successful" : "failure";
+    var testResultFile = fs.readFileSync("logs/" + testLink, "utf8");
+     fs.appendFileSync(serverLogFilePath, testResultFile);
+    if (testResultFile.indexOf("All files             |") > -1)
+      var coverage = testResultFile.substring(testResultFile.indexOf("All files             |") + 24, testResultFile.indexOf("|", testResultFile.indexOf("All files             |") + 24)).trim();
+    else
+      var coverage = "N/A";
     data.push({
       date: date,
       branch: branch,
@@ -54,10 +61,11 @@ app.get('/', function(req, res) {
       teststatus: teststatus,
       fuzzstatus: fuzzstatus,
       staticstatus: staticstatus,
-      buildlink: buildlink,
-      testlink: testlink,
-      fuzzlink: fuzzlink,
-      staticlink: staticlink
+      buildLink: buildLink,
+      testLink: testLink,
+      fuzzLink: fuzzLink,
+      staticLink: staticLink,
+      coverage: coverage
     });
   }
   res.render('index', {
@@ -128,29 +136,37 @@ app.post('/postreceive', function(req, res) {
   fs.appendFileSync(serverLogFilePath, 'POST request for /postreceive.\n');
 
   if (branch === "refs/heads/dev") {
+    var buildResult;
     fs.appendFileSync(buildLogPath, 'Will build dev branch.\n');
     try {
-      child = execSync("./scripts/build_dev");
-      fs.appendFileSync(buildLogPath, '\nOutput in stdout:\n ' + child + "\n");
+      child = execSync("./scripts/build_dev.sh");
+      fs.appendFileSync(buildLogPath, '\nOutput in stdout:\n ' + child);
       fs.appendFileSync(buildLogPath, 'dev branch build successful.\n');
-      if (runTests(testLogPath, "dev") &&
-        runFuzzingTests(fuzzingTestLogPath, "dev") &&
-        runStaticAnalysis(staticTestLogPath, "dev")) {
-        fs.appendFileSync(serverLogFilePath, 'dev branch build and tests successful.\n');
-        sendEmail(buildLogPath, "dev", true);
-        res.send('dev branch build and test successful.');
-      }
+      buildResult = true;
     } catch (error) {
-      fs.appendFileSync(serverLogFilePath, 'dev branch build or tests failed.\n');
+      fs.appendFileSync(serverLogFilePath, 'dev branch build failed.\n');
       fs.appendFileSync(buildLogPath, '\nexec error: \n' + error + "\n");
       fs.appendfilesync(buildLogpath, 'dev branch build error.\n');
-      sendEmail(buildLogPath, "dev", false);
-      res.send('dev branch build and test failed (check logs).');
+      buildResult = false;
     }
+
+    var testResults = true;
+    testResults = testResults && runTests(testLogPath, "dev");
+    testResults = testResults && runFuzzingTests(fuzzingTestLogPath, "dev");
+    testResults = testResults && runStaticAnalysis(staticTestLogPath, "dev");
+    if (testResults) {
+      fs.appendFileSync(serverLogFilePath, 'All dev branch tests successful.\n');
+    } else {
+      fs.appendFileSync(serverLogFilePath, 'Some dev branch test failed.\n');
+    }
+
+    sendEmail("dev", logPrefix, buildResult, testResults);
+
+    res.send('dev branch build and test complete. Check logs for results.');
   } else if (branch === "refs/heads/release") {
     fs.appendFileSync(buildLogPath, 'Will build release branch.\n');
     try {
-      child = execSync("./scripts/build_release");
+      child = execSync("./scripts/build_release.sh");
       fs.appendFileSync(buildLogPath, '\nOutput in stdout:\n ' + child + "\n");
       fs.appendFileSync(buildLogPath, 'release branch build successful.\n');
       if (runTests(testLogPath, "release") &&
@@ -164,7 +180,7 @@ app.post('/postreceive', function(req, res) {
       console.log("release build or tests failed");
       fs.appendFileSync(buildLogPath, '\nexec error: \n' + error + "\n");
       fs.appendfilesync(buildLogpath, 'release branch build error.\n');
-      sendEmail(buildLogPath, "release", false);
+      sendEmail(buildildLogPath, "release", false);
       res.send('release branch build and test failed (check logs).');
     }
   } else {
@@ -184,15 +200,35 @@ function getCurrentTimeInISO() {
 }
 
 //helper function to send emails
-function sendEmail(logFilePath, branch, buildSuccess) {
-  var execQuery = "./scripts/send_email " + logFilePath + " " + branch + " " + buildSuccess;
+function sendEmail(branch, logPrefix, buildResult, testResults) {
+  var buildLink = logPrefix + "_build.log";
+  var testLink = logPrefix + "_test.log";
+  var fuzzLink = logPrefix + "_fuzzingTest.log";
+  var staticLink = logPrefix + "_staticAnalysis.log";
+
+  var emailContent = "";
+
+  emailContent += "\n\nBuild log: \n[excerpt]\n";
+  emailContent += fs.readFileSync(buildLink, "utf8").trim().split("\n").slice(-20).join("\n");
+  emailContent += "\n\nTest log + Coverage report: \n[excerpt]\n";
+  emailContent += fs.readFileSync(testLink, "utf8").trim().split("\n").slice(-15).join("\n");
+  emailContent += "\n\nFuzzing log: \n[excerpt]\n";
+  emailContent += fs.readFileSync(fuzzLink, "utf8").trim().split("\n").slice(-15).join("\n");
+  emailContent += "\n\nStatic analysis log: \n[excerpt]\n";
+  emailContent += fs.readFileSync(staticLink, "utf8").trim().split("\n").slice(-15).join("\n");
+  emailContent += "\n\nGo to http://ec2-54-191-99-255.us-west-2.compute.amazonaws.com:3000/ to check detailed logs."
+
+  var emailLogFile = "logs/email.log";
+  fs.writeFileSync(emailLogFile, emailContent);
+
+  var execQuery = "./scripts/send_email.sh " + emailLogFile + " " + branch + " " + buildResult + " " + testResults;
   var child = exec(execQuery, function(error, stdout, stderr) {
     if (error !== null) {
-      fs.appendFileSync(logFilePath, "\n" + stderr);
-      fs.appendFileSync(logFilePath, "\nProblem sending email to admins.\n");
+      fs.appendFileSync(serverLogFilePath, "\n" + stderr);
+      fs.appendFileSync(serverLogFilePath, "\nProblem sending email to admins.\n");
     } else {
-      fs.appendFileSync(logFilePath, "\n" + stdout);
-      fs.appendFileSync(logFilePath, "\nEmail sent to admins.\n");
+      fs.appendFileSync(serverLogFilePath, "\n" + stdout);
+      fs.appendFileSync(serverLogFilePath, "\nEmail sent to admins.\n");
     }
   })
 }
---
+++ buildserver/scripts/send_email.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+echo "Running mail script."
+
+IFS=''
+
+while read line
+do
+echo "Sending mail to " $line
+if ($3 eq "true") then
+	subject='react-markdown '$2': build successful'
+else
+	subject='react-markdown '$2': build failed'
+fi
+if ($4 eq "true") then
+	subject=$subject' + tests passed'
+else
+	subject=$subject' + tests failed'
+fi
+cat $1 | mail -s $subject $line  
+done < ./scripts/email_ids
