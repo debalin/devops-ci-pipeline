91d8e6858ef8a4d0d77ddd1e24d56fd6554c8975
kkapoor <kkapoor@ncsu.edu>
2016-11-06 20:15:30 -0500

    checkout to fetch

--- buildserver/scripts/push_prod.sh
+++ buildserver/scripts/push_prod.sh
@@ -3,5 +3,5 @@
 
 echo "Pushing repository to production web server.\n"
 
-cd /home/ubuntu/markdown-js && GIT_WORK_TREE=/home/ubuntu/website-www/ git checkout -f
+cd /home/ubuntu/markdown-js && GIT_WORK_TREE=/home/ubuntu/website-www/ git fetch -f
 cd /home/ubuntu/website-www && npm install
