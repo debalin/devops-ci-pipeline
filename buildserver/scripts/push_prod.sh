d571e04c6577c59d7b718e63b112bef263671844
kkapoor <kkapoor@ncsu.edu>
2016-11-06 20:18:34 -0500

    forever process restart

--- buildserver/scripts/push_prod.sh
+++ buildserver/scripts/push_prod.sh
@@ -3,5 +3,7 @@
 
 echo "Pushing repository to production web server.\n"
 
+forever stop /home/ubuntu/website-www/webapp.js
 cd /home/ubuntu/markdown-js && GIT_WORK_TREE=/home/ubuntu/website-www/ git fetch -f
 cd /home/ubuntu/website-www && npm install
+forever start /home/ubuntu/website-www/webapp.js
